f19a65af19e6235c0eba86222c7a41de123687ab
brook8023 <brook8023@gmail.com>
2026-08-10 03:50:14 +0800

    second commit

--- traff_ip.sh
+++ traff_ip.sh
@@ -1,6 +1,8 @@
 #add docker
 apt update -y
-curl -s -L https://get.docker.com | LC_ALL=en_US.UTF-8 bash
+#curl -s -L https://get.docker.com | LC_ALL=en_US.UTF-8 bash
+apt install -y docker.io
+hostn=$(hostname)
 
 for i in $(seq 1 $1)
 do
@@ -9,5 +11,5 @@ do
 
     docker network create my_network_$i --driver bridge --subnet 192.168.$subnum.0/24
     iptables -t nat -I POSTROUTING -s 192.168.$subnum.0/24 -j SNAT --to-source 10.1.0.$iface
-    docker run -d --network my_network_$i --name tm_$i traffmonetizer/cli_v2 start accept --token $2
+    docker run -d --network my_network_$i --name tm_$i traffmonetizer/cli_v2 start accept --token $2 --device-name ${hostn}_$i
 done
